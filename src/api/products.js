df2b302431a4e802cf52cf9528f1c46cbae21d26
Juan Carlos Lloret <juancarloslloret93@gmail.com>
2019-01-20 18:20:03 +0100

    Added basic validation

--- src/api/products.js
+++ src/api/products.js
@@ -1,26 +1,44 @@
 const { Router } = require('express')
+
+const { validate, check } = require('../middleware/validation')
 const { Product } = require('../models')
 
 let products = Router()
 
-products.get('/small-appliances', async (req, res) => {
-  const { limit, offset } = req.query
-  const response = await Product.findAll({
-    where: { type: 'small-appliances' },
-    offset,
-    limit
-  })
-  res.json(response)
-})
+products.get(
+  '/small-appliances',
+  [
+    check('limit').isNumeric({ no_symbols: true }),
+    check('offset').isNumeric({ no_symbols: true })
+  ],
+  validate,
+  async (req, res) => {
+    const { limit, offset } = req.query
+    const response = await Product.findAll({
+      where: { type: 'small-appliances' },
+      offset,
+      limit
+    })
+    res.status(200).json(response)
+  }
+)
 
-products.get('/dishwashers', async (req, res) => {
-  const { limit, offset } = req.query
-  const response = await Product.findAll({
-    where: { type: 'dishwashers' },
-    offset,
-    limit
-  })
-  res.json(response)
-})
+products.get(
+  '/dishwashers',
+  [
+    check('limit').isNumeric({ no_symbols: true }),
+    check('offset').isNumeric({ no_symbols: true })
+  ],
+  validate,
+  async (req, res) => {
+    const { limit, offset } = req.query
+    const response = await Product.findAll({
+      where: { type: 'dishwashers' },
+      offset,
+      limit
+    })
+    res.status(200).json(response)
+  }
+)
 
 module.exports = products
